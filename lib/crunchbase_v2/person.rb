ec5883c0558d87274ea260d0c64af7b4dd5e093d
timakin <timaki.st@gmail.com>
2014-11-03 15:30:26 +0900

    person data can load

--- lib/crunchbase_v2/person.rb
+++ lib/crunchbase_v2/person.rb
@@ -2,11 +2,18 @@ module CrunchBaseV2
 	class Person < Common
 		OBJECT_NAME = 'person'
     attr_reader :first_name, :last_name, :permalink, :birthplace,
-      					:born_year, :born_month, :born_day, :born_on, :experience,
+      					:born_on_year, :born_month, :born_day, :born_on, :experience,
       					:founded_company, :degrees, :advisor_at, :investments
 
 		def initialize(json)
-			@permalink = json['data']['properties']['permalink']
+			@permalink 			 ||= json['data']['properties']['permalink']
+			@born_on 				 ||= json['data']['properties']['born_on']
+			@born_on_year 	 ||= json['data']['properties']['born_on_year']
+			@founded_company ||= json['data']['relationships']['founded_companies']
+			@experience 		 ||= json['data']['relationships']['experience']
+			@advisor_at  		 ||= json['data']['relationships']['advisor_at']
+			@degrees 				 ||= json['data']['relationships']['degrees']
+			@investments 		 ||= json['data']['relationships']['investments']
 		end
 	end
 end
