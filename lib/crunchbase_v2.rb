02cfc6e7fa3483d3d6eb58c3e9a3c52379063a4a
timakin <timaki.st@gmail.com>
2014-11-04 00:25:29 +0900

    organization data can load

--- lib/crunchbase_v2.rb
+++ lib/crunchbase_v2.rb
@@ -2,6 +2,7 @@ require "crunchbase_v2/version"
 require "crunchbase_v2/api"
 require "crunchbase_v2/common"
 require "crunchbase_v2/person"
+require "crunchbase_v2/organization"
 
 module CrunchbaseV2
 end
---
+++ lib/crunchbase_v2/organization.rb
@@ -0,0 +1,14 @@
+module CrunchBaseV2
+	class Organization < Common
+		OBJECT_NAME = 'organization'
+		attr_reader :total_funding_usd, :products, :news, :founded_on, :founded_on_year
+
+		def initialize(json)
+			@founded_on				 ||= json['data']['properties']['founded_on']
+			@founded_on_year   ||= json['data']['properties']['founded_on_year']
+			@total_funding_usd ||= json['data']['properties']['total_funding_usd']
+			@products 				 ||= json['data']['relationships']['products'] 
+			@news							 ||= json['data']['relationships']['news'] 
+		end
+	end
+end
