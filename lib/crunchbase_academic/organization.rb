2a66da50fe6bfe58b13eba0b2041f2a3d629d3fc
timakin <timaki.st@gmail.com>
2014-12-15 01:59:49 +0900

    funding rounds to org

--- lib/crunchbase_academic/organization.rb
+++ lib/crunchbase_academic/organization.rb
@@ -1,7 +1,7 @@
 module CrunchbaseAcademic
 	class Organization < Common
 		OBJECT_NAME = 'organization'
-		attr_reader :total_funding_usd, :products, :news, :founded_on, :founded_on_year
+		attr_reader :total_funding_usd, :products, :news, :founded_on, :founded_on_year, :funding_rounds
 
 		def initialize(json)
 			@founded_on				 ||= json['data']['properties']['founded_on']
@@ -9,6 +9,7 @@ module CrunchbaseAcademic
 			@total_funding_usd ||= json['data']['properties']['total_funding_usd']
 			@products 				 ||= json['data']['relationships']['products'] 
 			@news							 ||= json['data']['relationships']['news'] 
+			@funding_rounds		 ||= json['data']['relationships']['funding_rounds'] 
 		end
 	end
 end
